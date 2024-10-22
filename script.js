265ca632a7c2beb40ca58aed56c10976f6035510
Poushmita <poushmitap@gmail.com>
2024-10-22 15:37:10 +0530

    Added styles in Toggle

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//         const htmlElement = document.documentElement;
+//         const switchElement = document.getElementById('darkModeToggle');
+    
+//         // Set the default theme to dark if no setting is found in local storage
+//         const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+//         htmlElement.setAttribute('data-bs-theme', currentTheme);
+//         switchElement.checked = currentTheme === 'dark';
+    
+//         switchElement.addEventListener('change', function () {
+//             if (this.checked) {
+//                 htmlElement.setAttribute('data-bs-theme', 'dark');
+//                 localStorage.setItem('bsTheme', 'dark');
+//             } else {
+//                 htmlElement.setAttribute('data-bs-theme', 'light');
+//                 localStorage.setItem('bsTheme', 'light');
+//             }
+//         });
+//     });
